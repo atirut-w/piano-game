e41c5a77e0d82b24246ac1cac23ed782f45ae13b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-09-08 11:36:51 +0700

    Tweak async resource load function

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -14,9 +14,9 @@ func load_resource_async(path: String, type_hint: String = "") -> Resource:
 	var loader := ResourceLoader.load_interactive(path, type_hint)
 	var stage_count := loader.get_stage_count()
 
-	for n in stage_count + 1: # TODO: This looks sketchy as hell
+	for i in stage_count + 1: # TODO: This looks sketchy as hell
 		var err := loader.poll()
 		assert(err == OK or err == ERR_FILE_EOF)
-		yield(get_tree(), "idle_frame")
+		yield(get_tree(), "physics_frame")
 
 	return loader.get_resource()
