e3b1b50552b62dce380dc82b01822cb685609ce5
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-09-09 15:39:41 +0700

    Scale piano roll on windows resize

--- scenes/prtest/prtest.gd
+++ scenes/prtest/prtest.gd
@@ -2,6 +2,10 @@ extends Node
 
 
 func _ready() -> void:
+    get_viewport().connect("size_changed", self, "_scale_piano_roll")
+    _scale_piano_roll()
+
+func _scale_piano_roll() -> void:
     var roll := get_node("%piano_roll") as PianoRoll
     var screensize = get_viewport().size
 
