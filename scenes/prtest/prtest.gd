772e7d6d01c722daae598a88174e8d664f24f686
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-09-09 15:39:30 +0700

    Tweak piano roll scaling

--- scenes/prtest/prtest.gd
+++ scenes/prtest/prtest.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
     var roll := get_node("%piano_roll") as PianoRoll
     var screensize = get_viewport().size
 
-    roll.scale.x *= screensize.x / roll.size.x
+    roll.scale.x = screensize.x / roll.size.x
     roll.scale.y = roll.scale.x
 
     roll.position.y = screensize.y - (roll.size.y * roll.scale.y)
